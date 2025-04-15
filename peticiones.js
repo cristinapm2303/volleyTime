97af68c25e1f7e23d1948b850e624b08aa16e52f
manuguecor <mguerracordon@gmail.com>
2025-04-15 13:08:17 +0200

    Peticiones principales usuarios

--- peticiones.js
+++ peticiones.js
@@ -1,4 +1,5 @@
 const BASE_URL = "https://mock.apidog.com/m1/878633-860097-default";
+const USER_ID = 1;
 
 // 1. Obtener lista de eventos
 fetch(`${BASE_URL}/events`)
@@ -32,3 +33,74 @@ fetch(`${BASE_URL}/users/5/reservas`)
     console.log(data);
   })
   .catch(err => console.error("Error al obtener reservas:", err));
+
+// 4. Obtener lista de usuarios
+fetch(`${BASE_URL}/users`)
+  .then(res => res.json())
+  .then(data => {
+    console.log("Lista de usuarios:");
+    console.log(data);
+  })
+  .catch(err => console.error("Error al obtener usuarios:", err));
+
+// 5. Obtener usuario por ID
+fetch(`${BASE_URL}/users/${USER_ID}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log("Usuario:");
+    console.log(data);
+  })
+  .catch(err => console.error("Error al obtener el usuario:", err));
+
+// 6. Crear nuevo usuario
+fetch(`${BASE_URL}/users/new`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    "nombre": "Manuel",
+    "apellidos": "Guerra Cordón",
+    "nombre_usuario": "manugc",
+    "correo": "mguerracordon@gmail.com",
+    "contrasena": "1234"
+  })
+})
+  .then(res => res.json())
+  .then(data => {
+    console.log("Usuario creado:");
+    console.log(data);
+  })
+  .catch(err => console.error("Error al crear el usuario:", err));
+  
+// 7. Editar usuario
+fetch(`${BASE_URL}/users/edit/${USER_ID}`, {
+  method: "PUT",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    "nombre": "Manuel",
+    "apellidos": "Guerra Modificado",
+    "nombre_usuario": "manugcedit",
+    "correo": "mguerracordon@gmail.com",
+    "contrasena": "12345"
+  })
+})
+  .then(res => res.json())
+  .then(data => {
+    console.log("Usuario modificado:");
+    console.log(data);
+  })
+  .catch(err => console.error("Error al modificar el usuario:", err));
+
+// 8. Eliminar usuario
+fetch(`${BASE_URL}/users/delete/${USER_ID}`, {
+  method: "DELETE"
+})
+  .then(res => res.json())
+  .then(data => {
+    console.log("Usuario modificado:");
+    console.log(data);
+  })
+  .catch(err => console.error("Error al modificar el usuario:", err));
